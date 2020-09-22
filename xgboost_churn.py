849843aa0d53a2588fa33a22de1ab66af8c442cd
FrankDu <duyu12343@163.com>
2020-09-22 15:48:18 +0800

    Update xgb explainer

--- xgboost_churn.py
+++ xgboost_churn.py
@@ -34,7 +34,7 @@ def xgboost_churn(X_train, y_train, X_test, y_test):
     dtrain = xgb.DMatrix(X_train, y_train)
     lmda = 1.0
     params = {"objective": "binary:logistic", "max_depth": 6, "eta": 0.3, "gamma": 1,
-              "colsample_bytree": 0.8, "min_child_weight": 5, "subsample": 0.8}
+              "colsample_bytree": 0.8, "min_child_weight": 2, "subsample": 0.8}
     best_iteration = 150
     bst = xgb.train(params, dtrain, best_iteration)
 
@@ -65,40 +65,41 @@ def xgboost_churn(X_train, y_train, X_test, y_test):
     for i in range(len(importance_score_list)):
         print(real_fea_names[importance_score_list[i][1]] + '\t' + str(importance_score_list[i][0]))
     print("=" * 50)
-    top_20_f = []
-    for i in range(20):
-        top_20_f.append(real_fea_names[importance_score_list[i][1]])
+    top_12_f = []
+    for i in range(11):
+        top_12_f.append(real_fea_names[importance_score_list[i][1]])
+    top_12_f.append('CustomerCareCalls')
 
-    # # explainer model training
-    # params = {"objective": "binary:logistic", 'silent': 1, 'eval_metric': 'auc', 'base_score': 0.5, "lambda": lmda}
-    # bst = xgb.train(params, dtrain, best_iteration)
-    #
-    # # calculate xgboost probability
-    # tree_lst = xgb_exp.model2table(bst, lmda=lmda)
-    # leaf_lsts = bst.predict(dtrain, pred_leaf=True)
-    # fea_logit = [[] for _ in range(len(real_fea_names))]
-    # for i, leaf_lst in enumerate(leaf_lsts):
-    #     dist = xgb_exp.logit_contribution(tree_lst, leaf_lst)
-    #     for idx in range(len(real_fea_names)):
-    #         if 'f' + str(idx) in dist:
-    #             fea_logit[idx].append(-dist['f' + str(idx)])
-    #         else:
-    #             fea_logit[idx].append(0)
-    #
-    # # explainer plot
-    # fig = plt.figure(figsize=(30, 30))
-    # fig.suptitle('Feature User Churn')
-    # for i in range(len(top_20_f)):
-    #     fea = top_20_f[i]
-    #     idx = real_fea_names.index(fea)
-    #     fea_data = X_df[fea].values
-    #     ax = fig.add_subplot(5, 4, i + 1)
-    #     ax.set_xscale('log')
-    #     ax.set_xlim(min([i for i in fea_data if i > 0]), max(fea_data))
-    #     ax.scatter(fea_data, fea_logit[idx], s=0.1)
-    #     ax.hlines(0, min([i for i in fea_data if i > 0]), max(fea_data), linewidth=0.5)
-    #     ax.set_title(top_20_f[i])
-    # plt.subplots_adjust(wspace=0.3, hspace=0.3)
-    # plt.savefig('pic/' + name + '_' + str(best_iteration) + '.png', dpi=800)
+    # explainer model training
+    params = {"objective": "binary:logistic", 'silent': 1, 'eval_metric': 'auc', 'base_score': 0.5, "lambda": lmda}
+    bst = xgb.train(params, dtrain, best_iteration)
+
+    # calculate xgboost probability
+    tree_lst = xgb_exp.model2table(bst, lmda=lmda)
+    leaf_lsts = bst.predict(dtrain, pred_leaf=True)
+    fea_logit = [[] for _ in range(len(real_fea_names))]
+    for i, leaf_lst in enumerate(leaf_lsts):
+        dist = xgb_exp.logit_contribution(tree_lst, leaf_lst)
+        for idx in range(len(real_fea_names)):
+            if 'f' + str(idx) in dist:
+                fea_logit[idx].append(-dist['f' + str(idx)])
+            else:
+                fea_logit[idx].append(0)
+
+    # explainer plot
+    fig = plt.figure(figsize=(22, 20))
+    fig.suptitle('Feature User Churn')
+    for i in range(len(top_12_f)):
+        fea = top_12_f[i]
+        idx = real_fea_names.index(fea)
+        fea_data = X_df[fea].values
+        ax = fig.add_subplot(4, 3, i + 1)
+        # ax.set_xscale('log')
+        ax.set_xlim(min([i for i in fea_data if i > 0]), max(fea_data))
+        ax.scatter(fea_data, fea_logit[idx], s=0.1)
+        ax.hlines(0, min([i for i in fea_data if i > 0]), max(fea_data), linewidth=0.5)
+        ax.set_title(top_12_f[i])
+    plt.subplots_adjust(wspace=0.3, hspace=0.3)
+    plt.savefig('pic/' + name + '_' + str(best_iteration) + '_v3.png', dpi=800)
 
     return fpr, tpr, roc_auc
